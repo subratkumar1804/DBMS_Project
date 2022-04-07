2dddc75a562d18944776a1de6cd6dca50aac7225
SUBRAT KUMAR <68953830+subratkumar1804@users.noreply.github.com>
2022-04-07 16:08:03 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer -->
+            <footer>
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; ARS Ayush Rahul Subrat 2022</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
